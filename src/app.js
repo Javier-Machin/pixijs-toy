b6d8d5347c91a98f5604cc810e7f80b0d3083ae1
Javi M <javier_machin_acosta@hotmail.com>
2019-08-01 22:19:25 +0100

    feat: add basic game loop

--- src/app.js
+++ src/app.js
@@ -10,6 +10,10 @@ const Rectangle = PIXI.Rectangle;
 
 const app = initialize();
 
+// Here so it's in scope for the gameLoop
+let hero;
+let state;
+
 // Add texture atlas
 Loader.add('./spritesheets/hero.json').load(setup);
 
@@ -22,17 +26,41 @@ function setup() {
   const heroTextures = Resources['./spritesheets/hero.json'].textures;
 
   // Pick one texture and create a sprite from it
-  const hero = new Sprite(heroTextures['hero-down-2']);
+  hero = new Sprite(heroTextures['hero-down-2']);
 
   // Scale sprite
   hero.scale.set(3.5, 3.5);
 
   // Position sprite
   hero.position.set(255, 255);
+  // Standing still
+  hero.vx = 0;
+  hero.vy = 0;
 
   // Add sprite to the canvas
   app.stage.addChild(hero);
 
   // Render the stage
   app.renderer.render(app.stage);
+
+  // Set the game state
+  state = play;
+
+  // Start game loop
+  app.ticker.add(delta => gameLoop(delta));
+}
+
+function gameLoop(delta) {
+  state(delta);
+}
+
+function play(delta) {
+  // Update the hero's velocity
+  hero.vx = 1;
+  hero.vy = 1;
+
+  // Apply the velocity values to the hero's
+  // position to make it move
+  hero.x += hero.vx;
+  hero.y += hero.vy;
 }
